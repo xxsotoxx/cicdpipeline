83fd1eb7094b2b4ebe154b533ce1df83e7b5ab50
Anibal Soto <xxxxsotoxxxx@gmail.com>
2023-12-12 11:12:54 -0700

    initial commit

---
+++ src/test/java/com/example/cicdpipeline/CicdpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdpipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
